4aeda24937bed88adcd2c1ed9ef41edf3e4e9e18
IcemanMick <doylewithad@gmail.com>
2022-02-05 22:25:46 +0000

    add geography questions and answers array to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,44 @@
 window.onload = console.log('The window has loaded!');
+
+let questions = [
+    {
+        question: 'The area where a river meets the sea or ocean is called?',
+        choice1: 'An Estuary',
+        choice2: 'A Confluence',
+        choice3: 'A Source',
+        choice4: 'A Tributary',
+        answer: 1,
+    },
+    {
+        question: 'What is the driest desert in the world?',
+        choice1: 'Sahara',
+        choice2: 'Atacama',
+        choice3: 'Antarctica',
+        choice4: 'Gobi',
+        answer: 2,
+    },
+    {
+        question:'What is the deepest part of the ocean called?',
+        choice1: 'The Laurentian Abyss',
+        choice2: 'The Mariana Trench',
+        choice3: 'The Tonga Trench',
+        choice4: 'The Endless Void',
+        answer: 2,
+    },
+    {
+        question: 'What is the 2nd largest continent?',
+        choice1: 'Asia',
+        choice2: 'Antarctica',
+        choice3: 'South America',
+        choice4: 'Africa',
+        answer: 4,
+    },
+    {
+        question: 'How many time zones does Australia have?',
+        choice1: '1',
+        choice2: '2',
+        choice3: '3',
+        choice4: '4',
+        answer: 3,
+    }
+]
